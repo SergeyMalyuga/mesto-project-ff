d9e3ea537513741054c53faaabfb6c536db5e5bc
Sergey Malyuga <sergey@malyuga.ru>
2024-11-14 11:19:18 +0300

    Create function: createCard, removeCard, addCard

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,32 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+        name: "Архыз",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+        alt: "Горы покрытые зеленью и снегом у склонов"
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+        name: "Челябинская область",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+        alt: "Река на фоне зимнего леса"
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+        name: "Иваново",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+        alt: "Спальный район с высоты многоквартирного дома"
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+        name: "Камчатка",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+        alt: "Камчатские сопки"
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+        name: "Холмогорский район",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+        alt: "Железнодорожные пути среди леса"
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+        name: "Байкал",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+        alt: "Зимние Байкальские горы"
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+/**
+ *
+ * @param card Object card
+ * @param removeCard callback function to delete card
+ * @returns Object card
+ * @description create new Card
+ */
+function createCard(card, removeCard) {
+    const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+    newCard.querySelector('.card__image').src = card.link;
+    newCard.querySelector('.card__image').alt = card.alt;
+    newCard.querySelector('.card__title').textContent = card.name;
+    newCard.querySelector('.card__delete-button').addEventListener('click', removeCard);
+    return newCard;
+}
 
-// @todo: Функция создания карточки
+function removeCard(evt) {
+    const card = evt.target.closest('.card');
+    card.remove();
+}
 
-// @todo: Функция удаления карточки
+function addCard() {
+    const places = document.querySelector('.places__list');
+    initialCards.forEach((card) => places.append(createCard(card, removeCard)));
+}
 
-// @todo: Вывести карточки на страницу
+addCard();
